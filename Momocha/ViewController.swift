ded762ac1691f41cfa4f21c85fde6b1d37770327
sammanthp007 <samman.thapa@bison.howard.edu>
2017-03-01 21:10:20 -0500

    Initial Commit

---
+++ Momocha/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Momocha
+//
+//  Created by samman on 3/1/17.
+//  Copyright © 2017 keema. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
