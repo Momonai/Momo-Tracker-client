9f56bc7365c0b51c2549430e91e62998d83a8abc
sammanthp007 <samman.thapa@bison.howard.edu>
2017-04-06 17:34:46 -0400

    using swifty camera, DOESNT WORK

--- Momocha/CameraScreenViewController.swift
+++ Momocha/CameraScreenViewController.swift
@@ -7,15 +7,69 @@
 //
 
 import UIKit
+import SwiftyCam
 
-class CameraScreenViewController: UIViewController {
+class CameraScreenViewController: SwiftyCamViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, SwiftyCamViewControllerDelegate {
+    var first_time = true
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let captureButton = SwiftyCamButton(frame:CGRect(x:25, y:10, width: 100, height:150))
+        captureButton.delegate = self
+        
+        //first_time = true
+        
 
         // Do any additional setup after loading the view.
         // use this: SwiftyCamViewController
+
     }
+    
+    func swiftyCam(_ swiftyCam: SwiftyCamViewController, didTake photo: UIImage) {
+        // Called when takePhoto() is called or if a SwiftyCamButton initiates a tap gesture
+        // Returns a UIImage captured from the current session
+        print("clicked")
+    }
+    
+    
+    
+    func swiftyCam(_ swiftyCam: SwiftyCamViewController, didSwitchCameras camera: SwiftyCamViewController.CameraSelection) {
+        // Called when user switches between cameras
+        // Returns current camera selection
+        print("switched camera")
+    }
+    
+//    override func viewDidAppear(_ animated: Bool) {
+//        print ("####### came here")
+//        if (!first_time) {
+//            let vc = UIImagePickerController()
+//            vc.delegate = self
+//            vc.allowsEditing = true
+//            
+//            if UIImagePickerController.isSourceTypeAvailable(.camera) {
+//                vc.sourceType = .camera
+//            } else {
+//                vc.sourceType = .photoLibrary
+//            }
+//            
+//            self.present(vc, animated: true, completion: nil)
+//        } else {
+//            first_time = false
+//        }
+//        first_time = false
+//    }
+    
+    // delegate to handle the image
+//    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+//        print("came here with the image")
+//        let originalImage = info[UIImagePickerControllerOriginalImage] as! UIImage
+//        
+//        let editedImage = info[UIImagePickerControllerEditedImage] as! UIImage
+//        
+//        print("image taken and saved")
+//        // dismiss(animated: true, completion: nil)
+//    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
