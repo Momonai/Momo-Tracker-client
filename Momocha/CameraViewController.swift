e2653ef367b440cbf0dc45682f4409871724cd06
sammanthp007 <samman.thapa@bison.howard.edu>
2017-04-07 03:40:39 -0400

    figure out where to go after clicking image

--- Momocha/CameraViewController.swift
+++ Momocha/CameraViewController.swift
@@ -8,12 +8,39 @@
 
 import UIKit
 
-class CameraViewController: UIViewController {
+class CameraViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
 
+    var pickedImage: UIImage!
+    
+    var hasImage: Bool! = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+            print("camera screen accessed \n")
+            
+            let vc = UIImagePickerController()
+            vc.delegate = self
+            vc.allowsEditing = true
+            
+            if UIImagePickerController.isSourceTypeAvailable(.camera) {
+                vc.sourceType = .camera
+            } else {
+                vc.sourceType = .photoLibrary
+            }
+            
+            self.present(vc, animated: true, completion:nil)
+            
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        print("came her")
+        if hasImage {
+            print ("This is good")
+        } else {
+            print ("Not good")
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,15 +48,31 @@ class CameraViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
+    func imagePickerController(_ picker: UIImagePickerController,
+                               didFinishPickingMediaWithInfo info: [String : Any]) {
+        print("selected the image")
+        
+        // Get the image captured by the UIImagePickerController
+        let originalImage = info[UIImagePickerControllerOriginalImage] as! UIImage
+        let editedImage = info[UIImagePickerControllerEditedImage] as! UIImage
+        
+        pickedImage = editedImage
+        
+        
+        // Do something with the images (based on your use case)
+        hasImage = true
+        
+        // Dismiss UIImagePickerController to go back to your original view controller
+        dismiss(animated: true, completion: nil)
+        // performSegue(withIdentifier: "addDetailSegue", sender: self)
+    }
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
+        print("preparing for segue")
     }
-    */
 
 }
